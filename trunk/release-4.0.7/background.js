cc084b002e2223b402f518d4414ffc90795ddb75
jingzhao@google.com <jingzhao@google.com@a0d43ee6-04fa-a5ec-bf34-aeaa1c2c5ca5>
2011-05-13 02:01:40 +0000

    Release 4.0.7: Support BitComit in Windows.

---
+++ trunk/release-4.0.7/background.js
@@ -0,0 +1,178 @@
+var useContextMenuAPI = true;
+var enabledDownloaders = [];
+var plugin = document.getElementById('pluginId');
+
+chrome.extension.onRequest.addListener(function(request, sender, response) {
+  switch(request.msg) {
+  case 'init_loaded':
+    response({
+      'defaultDownloader': localStorage['defaultDownloader'],
+      'contextMenu': localStorage['contextMenu'],
+      'useContextMenuAPI': useContextMenuAPI,
+      'contextMenuList': enabledDownloaders});
+    break;
+  case 'thunder_windows':
+  case 'thunder_lite_windows':
+  case 'mini_thunder_windows':
+  case 'flashget_windows':
+  case 'mini_flashget_windows':
+  case 'qq_whirlwind_windows':
+  case 'emule_windows':
+  case 'orbit_windows':
+  case 'bitcomet_windows':
+  case 'idm_windows':
+  case 'fdm_windows':
+  case 'download_master_windows':
+  case 'getgo_windows':
+  case 'dap_windows':
+    downloaderManager.downloader[request.msg].download(request.link);
+    break;
+  case 'copyLink':
+    downloaderManager.copyLinkToClipboard(plugin, request.link.url);
+    break;
+  case 'contextMenu':
+    localStorage['contextMenu'] = 'false';
+    response({contextMenu: false});
+    break;
+  case 'downloadAll':
+    downloaderManager.downloader[request.downloader].downloadAll(
+        request.links, request.pageUrl);
+    break;
+  default:  // Linux downloaders
+    downloaderManager.downloader[request.msg].download(request.link);
+    break;
+  }
+});
+
+chrome.tabs.onSelectionChanged.addListener(function(tabId) {
+  updateEnabledDownloaders();
+  var js = 'chrome.tabs.executeScript(' + tabId + ', {file: "npdownload.js"});';
+  chrome.tabs.sendRequest(tabId, {
+    'msg': 'init_check',
+    'defaultDownloader': localStorage['defaultDownloader'],
+    'contextMenu': localStorage['contextMenu'],
+    'useContextMenuAPI': useContextMenuAPI,
+    'contextMenuList': enabledDownloaders
+  }, function(response) {
+    // If content script is loaded
+    if (response)
+      js = '';
+  });
+  setTimeout(function() {
+    eval(js);
+  }, 500);
+});
+
+function updateEnabledDownloaders() {
+  enabledDownloaders = downloaderManager.getEnabledDownloaders(plugin);
+  var numDownloaders = enabledDownloaders.length;
+  for (var i = 0; i < numDownloaders; i++)
+    if (enabledDownloaders[i].name == localStorage['defaultDownloader'])
+      break;
+  if (i == numDownloaders)
+    localStorage['defaultDownloader'] = 'chrome_downloader';
+  if (useContextMenuAPI) {
+    chrome.contextMenus.removeAll();
+    createContextMenu(plugin);
+  }
+}
+
+function getDefaultDownloadPath() {
+  return plugin.GetDefaultDownloadPath();
+}
+
+function openDownloadPath() {
+  var path = localStorage['downloadPath'];
+  plugin.OpenDownloadPath(path);
+}
+
+function openDownloadFilePath(filename) {
+  plugin.OpenDownloadFilePath(filename);
+}
+
+function updateDownloadPath() {
+  var path = localStorage['downloadPath'];
+  if (!path)
+    path = getDefaultDownloadPath();
+  plugin.UpdateDownloadPath(path);
+}
+
+function setDownloadPath() {
+  var path = localStorage['downloadPath'];
+  var title = chrome.i18n.getMessage('set_download_dialog_title');
+  return plugin.SetDownloadPath(title, path);
+}
+
+function createContextMenu(plugin) {
+  for (var i = 0; i < enabledDownloaders.length; i++) {
+    var downloader = enabledDownloaders[i];
+    if (!downloader.isSystem) {
+      var name = downloader.name;
+      var title =
+          chrome.i18n.getMessage(downloader.showName).replace(/%s/g, name);
+      contextMenuDownload(title, name, plugin);
+      if (downloader.supportDownloadAll) {
+        title = chrome.i18n.getMessage(downloader.showName2);
+        contextMenuDownloadAll(title, name);
+      }
+    }
+  }
+}
+
+function contextMenuDownload(title, downloader, plugin) {
+  chrome.contextMenus.create({title: title, contexts: ['link'],
+      onclick: function(info, tab) {
+    var link = {};
+    link.url = info.linkUrl;
+    link.text = info.selectionText || ' ';
+    link.pageUrl = info.pageUrl;
+    downloaderManager.downloader[downloader].download(link);
+  }}, function() {});
+}
+
+function contextMenuDownloadAll(title, downloader) {
+  chrome.contextMenus.create({title: title, contexts: ['all'],
+      onclick: function(info, tab) {
+    if (info.frameUrl) {
+      chrome.tabs.executeScript(null,
+          {code: 'npDownload.checkFrameByUrlAndDown("' +
+          info.frameUrl + '", "' + downloader + '")', allFrames: true})
+    } else {
+      chrome.tabs.executeScript(null,
+          {code: 'npDownload.sendDownloadAllCommandToBg("' +
+          downloader + '");'})
+    }
+  }});
+}
+
+function init() {
+  // Initialize setting in localStorage
+  localStorage['defaultDownloader'] =
+      localStorage['defaultDownloader'] || 'chrome_downloader';
+  localStorage['contextMenu'] = localStorage['contextMenu'] || 'true';
+  localStorage['rawCount'] = localStorage['rawCount'] || 0;
+
+  updateDownloadPath();
+  downloaderManager.init(plugin);
+
+  // Get supported downloaders list
+  enabledDownloaders = downloaderManager.getEnabledDownloaders(plugin);
+  if (useContextMenuAPI) {
+    createContextMenu(plugin);
+  }
+}
+
+var downloadFileName;
+
+function downloadCallback(retvalue, filename) {
+  downloadFileName = filename;
+  var notification = webkitNotifications.createHTMLNotification(
+      'notification.html');
+  notification.show();
+}
+
+function getFileName() {
+  return downloadFileName;
+}
+
+init();
---
+++ trunk/release-4.0.7/downloader.js
@@ -0,0 +1,557 @@
+/* Extend function  */
+function extend(subClass, superClass) {
+  var F = function() {};
+  F.prototype = superClass.prototype;
+  subClass.prototype = new F();
+  subClass.prototype.constructor = subClass;
+
+  subClass.superClass = superClass.prototype;
+  if (superClass.prototype.constructor == Object.prototype.constructor) {
+    superClass.prototype.constructor = superClass;
+  }
+}
+
+/* superClass */
+function Downloader(plugin) {
+  this.plugin = plugin;
+}
+
+Downloader.prototype.checkDownloader = function() {
+  return this.plugin.CheckObject(this.progId);
+}
+
+Downloader.prototype.updateNPObjectIfNeeded = function() {
+  if (!this.npObject)
+    this.npObject = this.plugin.CreateObject(this.progId);
+}
+
+Downloader.prototype.resetNPObject = function() {
+  this.npObject = null;
+}
+
+Downloader.prototype.download = function() {}
+
+Downloader.prototype.downloadAll = function() {}
+
+/* Thunder */
+function Thunder(plugin) {
+  Thunder.superClass.constructor.apply(this, arguments);
+  this.progId = 'ThunderAgent.Agent.1';
+}
+extend(Thunder, Downloader);
+
+Thunder.prototype.download = function(linkObj) {
+  this.npObject.AddTask5(
+      linkObj.url, '', '', '', '', -1, 0, -1, '', '', '', 1, '', -1);
+  this.npObject.CommitTasks2(1);
+}
+
+Thunder.prototype.downloadAll = function(links) {
+  for (var i = 0; i < links.length; i++) {
+    this.npObject.AddTask4(
+        links[i].url, '', '', links[i].text, '', -1, 0, -1, '', '', '');
+  }
+  this.npObject.CommitTasks2(1);
+}
+
+/* Thunder Lite */
+function ThunderLite(plugin) {
+  ThunderLite.superClass.constructor.apply(this, arguments);
+  this.progId = 'MiniThunderAgent.Agent.1';
+}
+extend(ThunderLite, Downloader);
+
+ThunderLite.prototype.download = function(linkObj) {
+  this.npObject.AddTask5(
+      linkObj.url, '', '', '', '', -1, 0, -1, '', '', '', 1, '', -1);
+  this.npObject.CommitTasks5(1, 1);
+}
+
+ThunderLite.prototype.downloadAll = function(links) {
+  for (var i = 0; i < links.length; i++) {
+    this.npObject.AddTask5(
+        links[i].url, '', '', links[i].text, '', -1, 0, -1, '', '', '', 1);
+  }
+  this.npObject.CommitTasks5(1, 1);
+}
+
+/* Mini Thunder */
+function MiniThunder(plugin) {
+  MiniThunder.superClass.constructor.apply(this, arguments);
+  this.progId = 'ToolbarThunder.DownloadAgent.1';
+}
+extend(MiniThunder, Downloader);
+
+MiniThunder.prototype.download = function(linkObj) {
+  this.npObject.AddTask(linkObj.url, '', '', '', '');
+}
+
+/* FlashGet */
+function Flashget(plugin) {
+  Flashget.superClass.constructor.apply(this, arguments);
+  this.progId = 'BHO.IFlashGetNetscapeEx';
+  this.version = '3.x';
+}
+extend(Flashget, Downloader);
+
+Flashget.prototype.checkDownloader = function() {
+  return this.plugin.CheckObject('BHO.IFlashGetNetscapeEx') ||
+      this.plugin.CheckObject('JetCar.Netscape');
+}
+
+Flashget.prototype.updateNPObjectIfNeeded = function() {
+  if (this.version == '3.x' && !this.plugin.CheckObject(this.progId)) {
+    this.progId = 'JetCar.Netscape';
+    this.version = '1.x';
+    this.npObject = null;
+  } else if (this.version == '1.x' &&
+      this.plugin.CheckObject('BHO.IFlashGetNetscapeEx')) {
+    this.progId = 'BHO.IFlashGetNetscapeEx';
+    this.version = '3.x';
+    this.npObject = null;
+  }
+  if (!this.npObject)
+    this.npObject = this.plugin.CreateObject(this.progId);
+}
+
+Flashget.prototype.download = function(linkObj) {
+  this.npObject.AddUrl(linkObj.url, linkObj.text, linkObj.pageUrl);
+}
+
+Flashget.prototype.downloadAll = function(links, pageUrl) {
+  var newLinks = [];
+  newLinks.push(pageUrl);
+  for (var i = 0; i < links.length; i++) {
+    var url = links[i].url;
+    var text = links[i].text;
+    newLinks.push(url);
+    newLinks.push(text);
+  }
+  if (this.version == '3.x')
+    this.npObject.AddAll(newLinks, '', "FlashGet3", 0, '');
+  else
+    this.npObject.AddUrlList(newLinks);
+}
+
+/* Mini FlashGet */
+function MiniFlashget(plugin) {
+  MiniFlashget.superClass.constructor.apply(this, arguments);
+  this.progId = 'BHO.IFlashGetNetscape';
+}
+extend(MiniFlashget, Downloader);
+
+MiniFlashget.prototype.download = function(linkObj) {
+  this.npObject.AddUrlEx(
+      linkObj.url, linkObj.text, '', "FlashGetMini", 0, '');
+}
+
+MiniFlashget.prototype.downloadAll = function(links, pageUrl) {
+  var newLinks = [];
+  newLinks.push(pageUrl);
+  for (var i = 0; i < links.length; i++) {
+    var url = links[i].url;
+    var text = links[i].text;
+    newLinks.push(url);
+    newLinks.push(text);
+  }
+  this.npObject.AddAll(newLinks, '', "FlashGetMini", 0, '');
+}
+
+/* QQ WhirlWind */
+function QQWhirlWind(plugin) {
+  QQWhirlWind.superClass.constructor.apply(this, arguments);
+  this.progId = 'QQIEHelper.QQRightClick.2';
+}
+extend(QQWhirlWind, Downloader);
+
+QQWhirlWind.prototype.download = function(linkObj) {
+  this.npObject.SendUrl2(
+      linkObj.url, linkObj.pageUrl, linkObj.text, '', 0, 0);
+}
+
+QQWhirlWind.prototype.downloadAll = function(links, pageUrl) {
+  for (var i = 0; i < links.length; i++) {
+    this.npObject.AddTask(links[i].url, pageUrl, links[i].text);
+  }
+  this.npObject.SendMultiTask();
+}
+
+/* EMule */
+function EMule(plugin) {
+  EMule.superClass.constructor.apply(this, arguments);
+  this.progId = 'IE2EM.IE2EMUrlTaker';
+}
+extend(EMule, Downloader);
+
+EMule.prototype.download = function(linkObj) {
+  this.npObject.SendUrl(linkObj.url, linkObj.text, linkObj.pageUrl);
+}
+
+/* Orbit */
+function Orbit(plugin) {
+  Orbit.superClass.constructor.apply(this, arguments);
+  this.progId = 'Orbitmxt.Orbit';
+}
+extend(Orbit, Downloader);
+
+Orbit.prototype.download = function(linkObj) {
+  this.npObject.download(linkObj.url, linkObj.text, linkObj.pageUrl, '', 0);
+}
+
+Orbit.prototype.downloadAll = function(links, pageUrl) {
+  var urls = [];
+  var texts = [];
+  for (var i = 0; i < links.length; i++) {
+    urls.push(links[i].url);
+    texts.push(links[i].text);
+  }
+  this.npObject.downloadList(urls, texts, pageUrl, "");
+}
+
+/* BitComet */
+function BitComet(plugin) {
+  BitComet.superClass.constructor.apply(this, arguments);
+  this.progId = 'BitCometAgent.BcAgent.1';
+}
+extend(BitComet, Downloader);
+
+BitComet.prototype.download = function(linkObj) {
+  var urls = [];
+  urls.push(linkObj.url);
+  urls.push(linkObj.text);
+  this.npObject.Download(urls, linkObj.pageUrl, "", "");
+}
+
+BitComet.prototype.downloadAll = function(links, pageUrl) {
+  var urls = [];
+  for (var i = 0; i < links.length; i++) {
+    urls.push(links[i].url);
+    urls.push(links[i].text);
+  }
+  this.npObject.Download(urls, pageUrl, "", "");
+}
+
+/* IDM */
+function IDM(plugin) {
+  IDM.superClass.constructor.apply(this, arguments);
+  this.progId = 'DownlWithIDM.LinkProcessor';
+}
+extend(IDM, Downloader);
+
+IDM.prototype.download = function(linkObj) {
+  this.npObject.Download(linkObj.url);
+}
+
+IDM.prototype.downloadAll = function(links, pageUrl) {
+  var urls = [];
+  var texts = [];
+  for (var i = 0; i < links.length; i++) {
+    urls.push(links[i].url);
+    texts.push(links[i].text);
+  }
+  this.npObject.DownloadAll(urls, texts, pageUrl);
+}
+
+/* FDM */
+function FDM(plugin) {
+  FDM.superClass.constructor.apply(this, arguments);
+  this.progId = 'WG.WGUrlReceiver';
+  this.progId2 = 'WG.WGUrlListReceiver';
+}
+extend(FDM, Downloader);
+
+FDM.prototype.updateNPObjectIfNeeded = function() {
+  // For Free Download Manager, create NPObject before downloading intead of
+  // create NPObject here, because unlike other downloaders, when its NPObject
+  // is created, its .exe executes at once.
+}
+
+FDM.prototype.resetNPObject = function() {
+  this.npObject = null;
+  this.npObjectAll = null;
+}
+
+FDM.prototype.download = function(linkObj) {
+  if (!this.npObject)
+    this.npObject = this.plugin.CreateObject(this.progId);
+  this.npObject.Url = linkObj.url;
+  this.npObject.Comment = linkObj.text;
+  this.npObject.Referer = linkObj.pageUrl;
+  this.npObject.Cookies = "";
+  this.npObject.AddDownload();
+}
+
+FDM.prototype.downloadAll = function(links, pageUrl) {
+  // Create new NPObject every time when downloading all links, 
+  // because there is no way to clear the url list from last time.
+  this.npObjectAll = this.plugin.CreateObject(this.progId2);
+  this.npObjectAll.Referer = pageUrl;
+  this.npObjectAll.Cookies = "";
+  for (var i = 0; i < links.length; i++) {
+    this.npObjectAll.Url = links[i].url;
+    this.npObjectAll.Comment = links[i].text;
+    this.npObjectAll.AddUrlToList();
+  }
+  this.npObjectAll.ShowAddUrlListDialog();
+}
+
+/* DownloadMaster */
+function DownloadMaster(plugin, progId) {
+  DownloadMaster.superClass.constructor.apply(this, arguments);
+  this.progId = 'dmie.moveurl';
+}
+extend(DownloadMaster, Downloader);
+
+DownloadMaster.prototype.download = function(linkObj) {
+  this.npObject.AddURL(linkObj.url, linkObj.pageUrl);
+}
+
+DownloadMaster.prototype.downloadAll = function(links, pageUrl) {
+  var urls = [];
+  for (var i = 0; l = links.length, i < l; i++) {
+    urls.push(links[i].url);
+  }
+  this.npObject.AddURLs(urls, pageUrl);
+}
+
+/* GetGo */
+function GetGo(plugin, progId) {
+  GetGo.superClass.constructor.apply(this, arguments);
+  this.progId = 'URLCatch.IEMenu';
+}
+extend(GetGo, Downloader);
+
+GetGo.prototype.download = function(linkObj) {
+  this.npObject.AddURL(linkObj.url);
+}
+
+GetGo.prototype.downloadAll = function(links, pageUrl) {
+  this.npObject.AddAllURL(pageUrl);
+}
+
+/* DAP */
+function DAP(plugin, progId) {
+  DAP.superClass.constructor.apply(this, arguments);
+  this.progId = 'dapie.catcher';
+}
+extend(DAP, Downloader);
+
+DAP.prototype.download = function(linkObj) {
+  this.npObject.MenuUrl2(linkObj.url, linkObj.pageUrl, '', linkObj.text);
+}
+
+/* Linux Downloader */
+function LinuxDownloader(plugin, command) {
+  LinuxDownloader.superClass.constructor.apply(this, arguments);
+  this.command = command;
+  this.progId = command.split(' ')[0];
+}
+extend(LinuxDownloader, Downloader);
+
+LinuxDownloader.prototype.download = function(linkObj) {
+  this.npObject.Download(linkObj.url, linkObj.pageUrl);
+}
+
+var downloaderManager = {};
+
+downloaderManager.menuItems = [
+  {
+    name: 'flashget_windows', showName: 'menu_flashget',
+    showName2: 'download_all_with_flashget',
+    privateLink: 'flashget://', isLinux: false,
+    supportDownloadAll: true, image: 'images/icon_flashget.png'
+  }, {
+    name: 'mini_flashget_windows', showName: 'menu_mini_flashget',
+    showName2: 'download_all_with_mini_flashget',
+    privateLink: 'flashget://', isLinux: false,
+    supportDownloadAll: true, image: 'images/icon_flashget.png'
+  }, {
+    name: 'thunder_windows', showName: 'menu_thunder',
+    showName2: 'download_all_with_thunder',
+    privateLink: 'thunder://', isLinux: false,
+    supportDownloadAll: true, image: 'images/icon_thunder.png'
+  }, {
+    name: 'thunder_lite_windows', showName: 'menu_thunder_lite',
+    showName2: 'download_all_with_thunder_lite',
+    privateLink: 'thunder://', isLinux: false,
+    supportDownloadAll: true, image: 'images/icon_thunderlite.png'
+  }, {
+    name: 'mini_thunder_windows', showName: 'menu_mini_thunder',
+    showName2: 'download_all_with_mini_thunder',
+    privateLink: 'thunder://', isLinux: false,
+    supportDownloadAll: false, image: 'images/icon_minithunder.png'
+  }, {
+    name: 'qq_whirlwind_windows', showName: 'menu_qq_whirlwind',
+    showName2: 'download_all_with_qq_whirlwind',
+    privateLink: '',isLinux: false,
+    supportDownloadAll: true, image: 'images/icon_qq.png'
+  }, {
+    name: 'emule_windows', showName: 'menu_emule',isLinux: false,
+    showName2: 'download_all_with_emule', privateLink: 'ed2k://',
+    supportDownloadAll: false, image: 'images/icon_emule.png'
+  }, {
+    name: 'orbit_windows', showName: 'menu_orbit',isLinux: false,
+    showName2: 'download_all_with_orbit', privateLink: '',
+    supportDownloadAll: true, image: 'images/icon_orbit.png'
+  }, {
+    name: 'bitcomet_windows', showName: 'menu_bitcomet',isLinux: false,
+    showName2: 'download_all_with_bitcomet', privateLink: '',
+    supportDownloadAll: true, image: 'images/icon_bitcomet.png'    
+  }, {
+    name: 'idm_windows', showName: 'menu_idm',isLinux: false,
+    showName2: 'download_all_with_idm', privateLink: '',
+    supportDownloadAll: true, image: 'images/icon_idm.png'
+  }, {
+    name: 'fdm_windows', showName: 'menu_fdm', isLinux: false,
+    showName2: 'download_all_with_fdm', privateLink: '',
+    supportDownloadAll: true, image: 'images/icon_fdm.png'
+  }, {
+    name: 'download_master_windows', showName: 'menu_download_master',
+    showName2: 'download_all_with_download_master', privateLink: '',
+    supportDownloadAll: true, image: 'images/icon_downloadmaster.png'
+  }, {
+    name: 'getgo_windows', showName: 'menu_getgo',
+    showName2: 'download_all_with_getgo', privateLink: '',
+    supportDownloadAll: true, image: 'images/icon_getgo.png'
+  }, {
+    name: 'dap_windows', showName: 'menu_dap',
+    showName2: 'download_all_with_dap', privateLink: '',
+    supportDownloadAll: false, image: 'images/icon_dap.png'
+  }, {
+    name: 'flashget_linux', showName: 'menu_flashget',
+    privateLink: '', isLinux: true, command: 'flashget $URL',
+    isUserAdded: false, supportDownloadAll: false,
+    image: 'images/icon_flashget_linux.png'
+  }, {
+    name: 'jdownloader', showName: 'menu_jdownloader',
+    privateLink: '', isLinux: true, command: 'jdownloader $URL',
+    isUserAdded: false, supportDownloadAll: false,
+    image: 'images/icon_jdownloader.png'
+  }, {
+    name: 'gwget', showName: 'menu_gwget', privateLink: '',
+    isLinux: true, command: 'gwget $URL', isUserAdded: false,
+    supportDownloadAll: false, image: 'images/icon_gwget.png'
+  }, {
+    name: 'aria2c', showName: 'menu_aria2c', privateLink: '',
+    isLinux: true, command: 'aria2c $URL', isUserAdded: false,
+    supportDownloadAll: false, image: 'images/icon_no_gui.png'
+  }, {
+    name: 'axel', showName: 'menu_axel', privateLink: '',
+    isLinux: true, command: 'axel $URL', isUserAdded: false,
+    supportDownloadAll: false, image: 'images/icon_no_gui.png'
+  }, {
+    name: 'curl', showName: 'menu_curl', privateLink: '',
+    isLinux: true, command: 'curl $URL', isUserAdded: false,
+    supportDownloadAll: false, image: 'images/icon_no_gui.png'
+  }, {
+    name: 'wget', showName: 'menu_wget', privateLink: '',
+    isLinux: true, command: 'wget $URL', isUserAdded: false,
+    supportDownloadAll: false, image: 'images/icon_no_gui.png'
+  }, {
+    name: 'chrome_downloader', showName: 'menu_chrome',
+    isSystem: true, supportDownloadAll: false,
+    image: 'images/icon_chrome.png'
+  }
+];
+
+downloaderManager.downloader = {};
+
+downloaderManager.init = function(plugin) {
+  // Create supported downloaders and save them in object
+  // downloaderManager.downloader
+  downloaderManager.downloader['thunder_windows'] =
+      new Thunder(plugin);
+  downloaderManager.downloader['mini_thunder_windows'] =
+      new MiniThunder(plugin);
+  downloaderManager.downloader['thunder_lite_windows'] = 
+      new ThunderLite(plugin);
+  downloaderManager.downloader['flashget_windows'] =
+      new Flashget(plugin);
+  downloaderManager.downloader['mini_flashget_windows'] =
+      new MiniFlashget(plugin);
+  downloaderManager.downloader['qq_whirlwind_windows'] =
+      new QQWhirlWind(plugin);
+  downloaderManager.downloader['emule_windows'] = new EMule(plugin);
+  downloaderManager.downloader['orbit_windows'] = new Orbit(plugin);
+  downloaderManager.downloader['bitcomet_windows'] = new BitComet(plugin);
+  downloaderManager.downloader['idm_windows'] = new IDM(plugin);
+  downloaderManager.downloader['fdm_windows'] = new FDM(plugin);
+  downloaderManager.downloader['download_master_windows'] =
+      new DownloadMaster(plugin);
+  downloaderManager.downloader['getgo_windows'] = new GetGo(plugin);
+  downloaderManager.downloader['dap_windows'] = new DAP(plugin);
+
+  for (var i = 0; i < downloaderManager.menuItems.length; i++) {
+    var item = downloaderManager.menuItems[i];
+    if (item.isLinux) {
+      // Create downloader in Linux platform and save them.
+      downloaderManager.downloader[item.name] =
+          new LinuxDownloader(plugin, item.command);
+    }
+  }
+
+  // Add user added downloaders
+  downloaderManager.addCustomDownloaders();
+}
+
+downloaderManager.addCustomDownloaders = function() {
+  for (var name in localStorage) {
+    if (name.indexOf('downloaderConfigure') == 0) {
+      var customArr = localStorage[name].split(',');
+      downloaderManager.menuItems.push({
+        storageName: name, name: customArr[1], showName: 'menu_custom',
+        isLinux: true, command: customArr[2], isUserAdded: true,
+        supportDownloadAll: false, image: customArr[0]});
+      downloaderManager.downloader[customArr[1]] =
+          new LinuxDownloader(plugin, customArr[2]);
+    }
+  }
+}
+
+downloaderManager.updateCustomDownloaders = function() {
+  for (var i = downloaderManager.menuItems.length - 1; i >= 0; i--) {
+    var item = downloaderManager.menuItems[i];
+    if (item.isUserAdded)
+      downloaderManager.menuItems.splice(i);
+  }
+  downloaderManager.addCustomDownloaders();
+}
+
+downloaderManager.getEnabledDownloaders = function(plugin) {
+  var enableMenuItems = [];
+  var last = 0;
+  for (var i = 0; i < downloaderManager.menuItems.length; i++) {
+    var item = downloaderManager.menuItems[i];
+    if (item.isSystem) {
+      // If the downloader is default downloader of Chrome
+      last = i;
+    } else {
+      var downloader = downloaderManager.downloader[item.name];
+      if (downloader.checkDownloader()) {
+        if (item.name == "mini_thunder_windows") {
+          var thunderLite = 
+              downloaderManager.downloader['thunder_lite_windows'];
+          if (thunderLite.checkDownloader())
+            continue;
+        }
+        
+        // If the downloader is available, update its NPObject
+        downloader.updateNPObjectIfNeeded();
+        enableMenuItems.push(item);
+      } else {
+        downloader.resetNPObject();
+      }
+    }
+  }
+  enableMenuItems.push(downloaderManager.menuItems[last]);
+  return enableMenuItems;
+}
+
+downloaderManager.removeDownloader = function(name) {
+  downloaderManager.downloader[name].resetNPObject();
+  delete downloaderManager.downloader[name];
+}
+
+downloaderManager.copyLinkToClipboard = function(plugin, url) {
+  plugin.CopyToClipboard(url);
+}
